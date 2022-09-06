4bd0440bf9bd7e3cf294573a76e1614cec15e681
PetrK39 <60184450+PetrK39@users.noreply.github.com>
2022-09-06 17:18:14 +0300

    Refactor Step 2: Reimplementation Fully reimplemented collapsible sidebar function Added customisation of the sidebar appearance Resizing the window now works fine The content of the sidebar is independent of the window size All changes are applied non-destructively by css classes, so they can be undone even without reloading the page

--- gelbooru-overhaul.user.js
+++ gelbooru-overhaul.user.js
@@ -9,6 +9,7 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_download
+// @grant       GM_addStyle
 // @require     https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
 // ==/UserScript==
 
@@ -17,15 +18,20 @@
     class ConfigManager {
         currentConfigVersion = 1;
         config;
+        constructor(){
+            this.loadConfig();
+        }
         loadConfig = function(){
             let cfg = GM_getValue("config", undefined);
 
             if(cfg == undefined){
                 this.config = this.getDefaultConfig();
+                this.saveConfig();
             }
             else if(this.currConfigVersion > cfg.configVersion) {
                 cfg = this.migrateConfig(cfg);
                 this.config = cfg;
+                this.saveConfig();
             }
             else {
                 this.config = cfg;
@@ -48,6 +54,12 @@
                     timeDarkModeForce: false,
                     timeDarkModeStartHour: 18,
                     timeDarkModeEndHour: 6,
+                },
+                collapseSidebar: {
+                    enabled: true,
+                    collapsedWidth: "5px",
+                    collapsedColor: "red",
+                    expandedOpacity: "90%",
                 }
             };
         }
@@ -75,62 +87,169 @@
         */
 
             // return updated config
-            this.saveConfig(migratingConfig)
             return migratingConfig;
         }
     }
-    
+
     const PageTypes = Object.freeze({GALLERY: "gallery", POST: "post", WIKI_VIEW: "wiki_view", POOL_VIEW: "pool_view", UNDEFINED: "undefined"});
-    
+
     let configManager = new ConfigManager();
-    configManager.loadConfig();
+
+    debugLog("Loaded config");
     debugLog(configManager.config);
-    
+
     // Any page tweaks
     debugLog("Current page type is " + detectPageType());
-    
+
+    debugLog("Registering styles");
+    registerStyles();
+
     // Page specific tweaks
     switch (detectPageType()){
         case PageTypes.GALLERY:
-        
-        break;
+            if(configManager.config.collapseSidebar.enabled) {
+                applyTweakCollapseSidebar();
+                applyFixCollapseSidebarGallery();
+            }
+            break;
         case PageTypes.POST:
-        
-        break;
+            if(configManager.config.collapseSidebar.enabled) {
+                applyTweakCollapseSidebar();
+                applyFixCollapseSidebarPost();
+            }
+            break;
         case PageTypes.WIKI_VIEW:
-        
-        break;
+
+            break;
         case PageTypes.POOL_VIEW:
-        
-        break;
+
+            break;
         case PageTypes.UNDEFINED:
-        
-        break;
+
+            break;
     }
-    
-    
+    // Register styles
+    function registerStyles(){
+        // applyTweakCollapseSidebar
+        GM_addStyle(`
+              .go-collapse-sidebar {
+                  position: fixed;
+                  width: ${configManager.config.collapseSidebar.collapsedWidth};
+                  height: 100%;
+                  margin: 0px;
+                  overflow: hidden;
+                  background: ${configManager.config.collapseSidebar.collapsedColor};
+                  transition: 142ms;
+                  font-size: 1em;
+                  z-index: 420690;
+              }
+              .go-collapse-sidebar:hover {
+                  position: fixed;
+                  width: min-content;
+                  height: 100%;
+                  padding-right: 12px;
+                  overflow-y: scroll;
+                  background: black;
+                  opacity: ${configManager.config.collapseSidebar.expandedOpacity};
+              }
+
+              /* add some top/bottom paddings */
+              #tag-list.go-tag-list-top-bottom-padding {
+                  padding-top: 12px;
+                  padding-bottom: 12px;
+              }
+
+              /* fix post list grid */
+              #container.go-collapse-sidebar-container-tweak {
+                  grid-template-columns: 0px auto;
+              }
+              @media only screen and (max-width: 850px) {
+                  #container.go-collapse-sidebar-container-tweak {
+                      grid-template-columns: auto;
+                  }
+              }
+
+              /* fix tag count wrap */
+              #tag-list li.go-collapse-sidebar-tags-list-tweak {
+                  width: max-content !important;
+                  display: block;
+              }
+
+              /* fix tag category hiding*/
+              @media only screen and (max-width: 850px) {
+                  .sm-hidden.go-sm-unhidden {
+                      display: contents;
+                  }
+              }
+
+              /* fix mobile spacing */
+              .go-mobile-unspacing {
+                  margin-right: 0;
+              }
+        `);
+
+
+    }
+
+    // Apply Tweak
+    function applyTweakCollapseSidebar(){
+        debugLog("Applying TweakCollapseSidebar");
+        onDOMReady(()=>{
+            document.querySelector("#container > section").classList.add("go-collapse-sidebar");
+            document.querySelector("#tag-list").classList.add("go-tag-list-top-bottom-padding");
+        });
+    }
+    function applyFixCollapseSidebarGallery(){
+        debugLog("Applying FixCollapseSidebarGallery");
+        onDOMReady(()=>{
+            document.querySelectorAll("#tag-list > li").forEach((i) => {i.classList.add("go-collapse-sidebar-tags-list-tweak");});
+            document.querySelector("#container").classList.add("go-collapse-sidebar-container-tweak");
+            document.querySelectorAll("#tag-list > li > a.mobile-spacing").forEach((i)=>{i.classList.add("go-mobile-unspacing")});
+            document.querySelectorAll(".aside > .sm-hidden").forEach((i)=>{i.classList.add("go-sm-unhidden")});
+        });
+    }
+    function applyFixCollapseSidebarPost(){
+        debugLog("Applying FixCollapseSidebarPost");
+        onDOMReady(()=>{
+            document.querySelectorAll("#tag-list > li[class*='tag-type']").forEach((i) => {i.classList.add("go-collapse-sidebar-tags-list-tweak");});
+            document.querySelector("#container").classList.add("go-collapse-sidebar-container-tweak");
+            document.querySelectorAll("#tag-list > .sm-hidden").forEach((i)=>{i.classList.add("go-sm-unhidden")});
+            document.querySelectorAll("#tag-list > li > .sm-hidden").forEach((i)=>{i.classList.add("go-sm-unhidden")});
+        });
+    }
+
     // Functions
     function detectPageType(){
         let params = new URLSearchParams(document.URL.split('?')[1]);
-        
+
         if(!params.has("page"))
             return PageTypes.UNDEFINED;
-        
+
         if(params.get("page") == "post" && params.get("s") == "list")
             return PageTypes.GALLERY;
-        
+
         if(params.get("page") == "post" && params.get("s") == "view")
             return PageTypes.POST;
-        
+
         if(params.get("page") == "wiki" && params.get("s") == "view")
             return PageTypes.WIKI_VIEW;
-        
+
         if(params.get("page") == "pool" && params.get("s") == "show")
             return PageTypes.POOL_VIEW;
-        
+
         return PageTypes.UNDEFINED;
     }
-    
+
+    function onDOMReady(func){
+        if (document.readyState == "loaded" || document.readyState == "interactive" || document.readyState == "complete") {
+            func();
+        } else {
+            document.addEventListener("DOMContentLoaded", function(event) {
+                func();
+            });
+        }
+    }
+
     function debugLog(value){
         // Notice no debug until configManager loades and migrates config
         // Probably im should add force debug while migrating config...
