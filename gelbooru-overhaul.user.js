ffd39f6622d501f3cc1e755c87c510688e038d4b
Encho <38841444+Enchoseon@users.noreply.github.com>
2023-05-10 05:47:26 +0000

    Bump script version
    Just to ping any people to update.

--- gelbooru-overhaul.user.js
+++ gelbooru-overhaul.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Gelbooru Overhaul
 // @namespace   https://github.com/Enchoseon/gelbooru-overhaul-userscript/raw/main/gelbooru-overhaul.user.js
-// @version     0.7.2
+// @version     0.9.9
 // @description Various toggleable changes to Gelbooru such as enlarging the gallery, removing the sidebar, and more.
 // @author      Enchoseon
 // @include     *gelbooru.com*
